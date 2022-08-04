f9f2aebc71a6230aefec0c3120295d248e329b54
Vov4ik3587 <galstyan.vova@gmail.com>
2022-08-04 16:31:56 +0700

    сделал валидацию для всего, кроме date

--- feedback.php
+++ feedback.php
@@ -1,7 +1,4 @@
 <?php
-// старт сессии
-session_start();
-
 // подключаем бд
 $host = '127.0.0.1';
 $db = 'clothes_shop';
@@ -100,7 +97,7 @@ $pdo = new PDO($dsn, $user, $password, $opt);
                 </label>
             </div>
             <div class="block-form__buttons">
-                <input type="submit" value="Отправить">
+                <input type="submit" name="submit" value="Отправить">
                 <input type="reset" value="Очистить">
             </div>
         </form>
@@ -110,23 +107,55 @@ $pdo = new PDO($dsn, $user, $password, $opt);
 </html>
 
 <?php
-$name = $_GET['first-name'] ?? null;
-$email = $_GET['email'] ?? null;
-$date = $_GET['date'] ?? null;
-$sex = $_GET['sex'] ?? null;
-$theme = $_GET['theme-appeal'] ?? null;
-$text = $_GET['text-appeal'] ?? null;
+$name = $_GET['first-name'];
+$email = $_GET['email'];
+$date = $_GET['date'];
+$sex = $_GET['sex'];
+$theme = $_GET['theme-appeal'];
+$text = $_GET['text-appeal'];
 
 setcookie("name", $name);
 setcookie("email", $email);
 setcookie("date", $date);
 setcookie("sex", $sex);
 
-$stmt = $pdo->prepare('
-    INSERT INTO clothes_shop.feedback (name, email, date, sex, theme, text)
-    VALUES (?, ?, ?, ?, ?, ?)
-');
-$stmt->execute([$name, $email, $date, $sex, $theme, $text]);
+$valid_form = true;
 
-header("Location: http://localhost/");
-?>
+if (isset($_GET['submit'])) {
+    if (!isset($name) || !preg_match('/^[a-zA-Zа-яА-Я]+$/ui',$name)) {
+        echo "Неправильно введено имя <br>";
+        $valid_form = false;
+    }
+
+    if(!isset($email) || !preg_match('/\w+@\w+\.\w+/', $email)) {
+        echo "Неправильно введен email(введите в формате user@email.domen) <br>";
+        $valid_form = false;
+    }
+    // дата должна быть между 1900-01-01 и 2099-12-31, но не работает
+//    if(!isset($date) || !preg_match('/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/', $date)) {
+//        echo "Такой даты не может быть <br>";
+//        $valid_form = false;
+//    }
+
+    if(!isset($theme) || !preg_match('/[a-zA-Zа-яА-Я0-9 ]{2,49}/', $theme)) {
+        echo "Введены недопустимые символы в теме обращения <br>";
+        $valid_form = false;
+    }
+
+    if(!isset($text) || !preg_match('/[a-zA-Zа-яА-Я0-9 ]{2,1024}/', $text)) {
+        echo "Введены недопустимые символы в тексте обращения <br>";
+        $valid_form = false;
+    }
+
+    if ($valid_form) {
+        $stmt = $pdo->prepare('
+            INSERT INTO clothes_shop.feedback (name, email, date, sex, theme, text)
+            VALUES (?, ?, ?, ?, ?, ?)
+        ');
+        $stmt->execute([$name, $email, $date, $sex, $theme, $text]);
+
+        header("Location: http://localhost/");
+    } else {
+        echo "Форма не валидна";
+    }
+}
